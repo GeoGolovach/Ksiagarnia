dc96cd58980851a9501d687b5576675037dddbdf
GeoGolovach <16102007georg@mail.ru>
2025-02-06 18:44:40 +0100

    update 2.0

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mysql = require('mysql2');
 const bcrypt = require("bcrypt");
 const session = require('express-session');
 const multer = require('multer');
+const path = require('path');
 const app = express();
 const { Book, books } = require('./public/models/book');
 
+// Настройка сервера и подключение к БД
 
 const connection = mysql.createConnection({
     host : 'localhost',
@@ -27,6 +29,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static('public'))
+app.use('/uploads', express.static('uploads'));
 
 app.use(session({
     secret: '8hD$3pQ!tV@9xW&7zJ*2kR#5fL%1mN^4y',
@@ -37,14 +40,145 @@ app.use(session({
     }
 }));
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Уникальное имя файла
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png/; // Разрешенные типы файлов
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    
+    if (mimetype && extname) {
+        return cb(null, true);
+    }
+    cb(new Error('Ошибка: Неподдерживаемый тип файла!'));
+};
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 4 * 1024 * 1024}, // Ограничение на размер файла (4 МБ)
+    fileFilter: fileFilter
+});
+
+// Функции, методы и пути
+
+async function getBooksByGenre(genre) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM books WHERE genre = ? ORDER BY name ASC;', [genre], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results);
+        })
+    })
+}
+
+async function getBooks(page = 1, limit = 15) {
+    const offset = (page - 1) * limit;
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM books LIMIT ?, ?', [offset, limit], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
+
+async function getUsers() {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM users', (err, result) => {
+            if (err) {
+                return reject(err);
+            }   else {
+                resolve(result);
+            }
+        });
+    });
+}
+
+async function getBookById(id) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM books WHERE id = ?;', [id], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results[0]); // Возвращаем первую книгу
+        });
+    });
+}
+
+async function getOrdersByUserId(userId) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM orders WHERE user_id = ?;', [userId], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
+
+async function getWishlistByUserId(userId) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM wishlist WHERE user_id = ?;', [userId], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
+
+async function getBooksByIds(bookIds) {
+    if (!bookIds.length) {
+        return []; // Возвращаем пустой массив, если bookIds пустой
+    }
+
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM books WHERE id IN (?);', [bookIds], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
+
+
+
 app.get('/', async (req, res) => {
+    const page = req.query.page || 1;
     try {
-        const books = await getBooks();
+        const books = await getBooks(page);
         const user = req.session.user || null; // Получаем информацию о пользователе из сессии
-        res.render('glownastrona', { books, user });
+        res.render('glownastrona', { books, user, currentPage: page });
     }catch (err) {
         console.error(err);
         res.status(500).send('Server error');
+    }
+});
+
+app.post('/search', async (req, res) => {
+    const search = req.body.search;
+    try {
+        connection.query('SELECT * FROM books WHERE name LIKE ? OR author LIKE ?;', [`%${search}%`, `%${search}%`], (err, results) => {
+            if (err) {
+                console.log(err);
+            }   else {
+                res.json(results);
+            }
+        })
+    }
+    catch (error) {
+        console.error('Ошибка поиска книг:', error);
+        res.status(500).send('Ошибка поиска книг');
     }
 });
 
@@ -65,7 +199,8 @@ app.get('/book1/:id', async (req, res) => {
         try {
             const book = await getBookById(bookId);
             if (book) {
-                res.render('book1', { book, user });
+                const similarBooks = await getBooksByGenre(book.genre);
+                res.render('book1', { book, user, similarBooks });
             } else {
                 res.status(404).send('Книга не найдена');
             }
@@ -75,17 +210,6 @@ app.get('/book1/:id', async (req, res) => {
         }
     }
 });
-
-async function getBookById(id) {
-    return new Promise((resolve, reject) => {
-        connection.query('SELECT * FROM books WHERE id = ?;', [id], (err, results) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(results[0]); // Возвращаем первую книгу
-        });
-    });
-}
 
 app.get('/admin', (req, res) => {
     const user = req.session.user || null;
@@ -137,13 +261,18 @@ app.post('/data-form-logowanie', (req, res) => {
                         id: result[0].id,
                         name : result[0].name,
                         email: result[0].email,
-                        isAdmin: isAdmin
+                        isAdmin: isAdmin,
+                        phone: result[0].phone,
+                        address: result[0].address,
+                        filePath: result[0].avatar_path
                     };
 
                     if (isAdmin) {
                         return res.redirect('/admin');
                     }   else {
+                        console.log(req.session.user);
                         return res.redirect('/');
+                        
                     }
                     
                 } else {
@@ -246,72 +375,293 @@ app.post('/data-form-rejestrowanie', (req, res) => {
             });
     });
 
-    async function getBooks() {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT * FROM books', (err, results) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(results);
-            });
-        });
-    }
-
-    async function getUsers() {
-        return new Promise((resolve, reject) => {
-            connection.query('SELECT * FROM users', (err, result) => {
-                if (err) {
-                    return reject(err);
-                }   else {
-                    resolve(result);
-                }
-            });
-        });
-    }
-
-app.get('/profile', (req, res) => {
+app.get('/profile', async (req, res) => {
     const user = req.session.user || null;
     if (!user) {
         return res.redirect('/zalogowanie');
-    }   else {
-    res.render('profile', { user });
     }
-})
+    
+    try {
+        const orders = await getOrdersByUserId(user.id);
+        const bookIds = orders.map(order => order.book_id);
+        const books = await getBooksByIds(bookIds);
+        const wishlist = await getWishlistByUserId(user.id);
+        
+        // Передаем в шаблон как orders, так и books
+        res.render('profile', { user, orders, books, wishlist});
+    } catch (error) {
+        console.log('Заказов нет');
+        res.render('profile', { user, orders: [], books: [], wishlist: []});
+    }
+});
 
-app.get('/orders', async(req, res) => {
+app.get('/orders', async (req, res) => {
+    const user = req.session.user || null;
+    if (!user) {
+        return res.redirect('/zalogowanie');
+    }
+    
+    try {
+        const orders = await getOrdersByUserId(user.id);
+        
+        // Проверяем, есть ли заказы
+        if (!orders.length) {
+            return res.json({ orders: [], books: [] });
+        }
+        
+        const bookIds = orders.map(order => order.book_id);
+        const books = await getBooksByIds(bookIds);
+        
+        res.json({ orders, books });
+    } catch (error) {
+        console.error('Ошибка получения заказов:', error);
+        res.status(500).send('Ошибка получения заказов');
+    }
+});
+
+app.get('/wishlist', async (req, res) => {
     const user = req.session.user || null;
     if (!user) {
         return res.redirect('/zalogowanie');
     }   else {
         try {
-            const orders = await db.getOrdersByUserId(userId); // Получите заказы из базы данных
-            res.json(orders);
-        } catch (error) {
-            res.status(500).send('Ошибка получения заказов');
+            const wishlist = await getWishlistByUserId(user.id);
+            const bookIds = wishlist.map(wish => wish.book_id);
+            const books = await getBooksByIds(bookIds);
+            res.json({ user, wishlist, books });
         }
+        catch (error) {
+            console.error('Ошибка получения списка желаемого:', error);
+            res.status(500).send('Ошибка получения списка желаемого');
+        }
+
     }
 })
 
-app.post('/add-order', async (req, res) => {
-    const { bookId, userId } = req.body;
+app.post('/orders/add-to-cart', async (req, res) => {
+    const { bookId } = req.body;
+    const user = req.session.user; // Получаем текущего пользователя
+
+    if (!user) {
+        return res.status(401).send('Пожалуйста, войдите в систему'); // Неавторизованный доступ
+    }
+
+    const userId = user.id; // Получаем ID текущего пользователя
+
     try {
-        await db.addOrder(bookId, userId); // Добавьте заказ в базу данных
-        res.json({ message: 'Заказ добавлен' });
+        const sql = 'INSERT INTO orders (user_id, book_id) VALUES (?, ?);';
+        connection.query(sql, [userId, bookId], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Ошибка добавления заказа');
+            }
+            res.send('Книга добавлена в корзину'); // Успешное сообщение
+            
+        });
     } catch (error) {
         res.status(500).send('Ошибка добавления заказа');
     }
 });
 
-async function getOrdersByUserId(userId) {
-    return new Promise((resolve, reject) => {
-        connection.query('SELECT * FROM orders WHERE user_id = ?;', [userId], (err, results) => {
+app.post('/wishlist/add-to-wishlist', async (req, res) => {
+    const { bookId } = req.body;
+    const user = req.session.user; // Получаем текущего пользователя
+
+    if (!user) {
+        return res.status(401).send('Пожалуйста, войдите в систему'); // Неавторизованный доступ
+    }   else {
+        const userId = user.id;
+        try {
+            const sql = 'INSERT INTO wishlist (user_id, book_id) VALUES (?, ?);';
+            connection.query(sql, [userId, bookId], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Ошибка добавления в список желаемого');
+                }
+                console.log('Книга добавлена в список желаемого');
+                res.send('Книга добавлена в список желаемого');
+            })
+        }
+        catch (error) {
+            res.status(500).send('Ошибка добавления в список желаемого');
+        }
+    }
+
+})
+
+app.post('/wishlist/remove-from-wishlist', async (req, res) => {
+    const { bookId } = req.body;
+    const user = req.session.user; // Получаем текущего пользователя
+
+    if (!user) {
+        return res.status(401).send('Пожалуйста, войдите в систему'); // Неавторизованный доступ
+    }   else {
+        const userId = user.id;
+        try {
+            const sql = 'DELETE FROM wishlist WHERE user_id = ? AND book_id = ?;';
+            connection.query(sql, [userId, bookId], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Ошибка удаления из списка желаемого');
+                }
+                console.log('Книга удалена из списка желаемого');
+                res.json({ success: true, message: 'Книга удалена из списка желаемого' });
+            })
+        }
+        catch (error) {
+            res.status(500).send('Ошибка удаления из списка желаемого');
+        }
+    }
+});
+
+app.post('/orders/remove-from-orders', async (req, res) => {
+    const { bookId } = req.body;
+    const user = req.session.user; // Получаем текущего пользователя
+
+    if (!user) {
+        return res.status(401).send('Пожалуйста, войдите в систему'); // Неавторизованный доступ
+    }   else {
+        const userId = user.id;
+        try {
+            const sql = 'DELETE FROM orders WHERE user_id = ? AND book_id = ?;';
+            connection.query(sql, [userId, bookId], (err, result) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Ошибка удаления из корзины');
+                }
+                console.log('Книга удалена из корзины');
+                res.json({ success: true, message: 'Книга удалена из корзины' });
+            })
+        }
+        catch (error) {
+            res.status(500).send('Ошибка удаления из корзины');
+        }
+    }
+});
+
+// Изменения профиля и обновление данных
+
+app.post('/update-profile', async (req, res) => {
+    const { username, email, phone, address } = req.body;
+    const user = req.session.user; // Получаем текущего пользователя
+
+    if (!user) {
+        return res.status(401).send('Пожалуйста, войдите в систему'); // Неавторизованный доступ
+    }
+
+    const userId = user.id;
+
+    // Создаем массив для хранения обновлений
+    const updates = [];
+    const values = [];
+
+    // Проверка и добавление обновлений для каждого поля
+    if (username) {
+        updates.push('name = ?');
+        values.push(username);
+    }
+    if (email) {
+        updates.push('email = ?');
+        values.push(email);
+    }
+    if (phone) {
+        updates.push('phone = ?');
+        values.push(phone);
+    }
+    if (address) {
+        updates.push('address = ?');
+        values.push(address);
+    }
+
+    // Добавляем userId в конце массива значений
+    values.push(userId);
+
+    // Если не было обновлений, возвращаем ошибку
+    if (updates.length === 0) {
+        return res.status(400).send('Нет данных для обновления');
+    }
+
+    // Собираем SQL-запрос
+    const sql = `UPDATE users SET ${updates.join(', ')} WHERE id = ?`;
+
+    try {
+        connection.query(sql, values, (err, result) => {
             if (err) {
-                return reject(err);
+                console.error(err);
+                return res.status(500).send('Ошибка обновления профиля');
             }
-            resolve(results);
+            console.log('Профиль обновлен');
+            res.redirect('/profile?section=settings');  
         });
-    });
-}
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Ошибка обновления профиля');
+    }
+});
+
+// Категории книг 
+
+app.post('/postBooksByCategory', async (req, res) => {
+    const { category } = req.body;
+    try {
+        connection.query('SELECT * FROM books WHERE genre = ? ORDER BY name ASC;', [category], (err, results) => {
+            if (err) {
+                console.log(err);
+            }   else {
+                res.json(results);
+            }
+        })
+    } catch (error) {
+        console.error('Ошибка получения книг по категории:', error);
+        res.status(500).send('Ошибка получения книг по категории');
+    }
+});
+
+// Работа с изображениями, аватарами и файлами
+
+app.post('/upload-avatar', upload.single('avatar'), (req, res) => {
+    const user = req.session.user || null;
+    if (!user) {
+        return res.status(401).send('Пожалуйста, войдите в систему');
+    }
+
+    if (!req.file) {
+        return res.status(400).send('Ошибка загрузки файла');
+    }
+    const filePath = req.file.path;
+    console.log('Файл загружен:', filePath);
+    const userId = user.id;
+
+    try {
+        connection.query('UPDATE users SET avatar_path = ? WHERE id = ?;', [filePath, userId], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Ошибка обновления аватара');
+            }
+            console.log('Аватар обновлен');
+            
+            res.json({ success: true, message: 'Аватар обновлен', filePath });
+        });
+    }
+    catch (error) {
+        console.error('Ошибка обновления аватара:', error);
+        res.status(500).send('Ошибка обновления аватара');
+    } 
+});
+
+// Разлогинивание
+
+app.post('/logout', async (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Ошибка сервера');
+        }   else {
+            res.redirect('/');
+        }}); 
+})
+
+
 
 
 
